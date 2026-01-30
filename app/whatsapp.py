f79d3e8c6a2e77727310288e70de6623beec747c
BhavithaNaramamidi <111362894+BhavithaNaramamidi@users.noreply.github.com>
2026-01-31 02:33:56 +0530

    Update whatsapp.py

--- app/whatsapp.py
+++ app/whatsapp.py
@@ -1,4 +1,15 @@
-def send_whatsapp_message(user_id, message):
-    print(f"\n--- WhatsApp message to {user_id} ---")
-    print(message)
-    print("----------------------------------\n")
+import os
+from twilio.rest import Client
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+from_number = os.getenv("TWILIO_WHATSAPP_FROM")
+
+client = Client(account_sid, auth_token)
+
+def send_whatsapp_message(to, body):
+    client.messages.create(
+        from_=from_number,
+        to=to,
+        body=body
+    )
